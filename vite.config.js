d76252ed2404494afa087c0bf4a0f17e7d37c2e7
KreutzerCode <KreutzerCode@gmail.com>
2023-03-05 11:49:19 +0100

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import vitePluginString from 'vite-plugin-string'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte(),vitePluginString()],
+})
